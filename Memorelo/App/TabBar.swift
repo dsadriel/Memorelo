b3e622fb78b28ffa7d13224e49edd90e930c9d47
Adriel de Souza <ds.adriel@gmail.com>
2025-08-12 10:43:09 -0300

    Refactor TabBar to improve loose attachment cleanup; remove redundant onAppear logic and enhance file management

--- Memorelo/App/TabBar.swift
+++ Memorelo/App/TabBar.swift
@@ -10,16 +10,48 @@ import SwiftData
 
 struct TabBar: View {
     @Environment(\.modelContext) var modelContext
-    
-    @Query(filter: #Predicate<MemoryAttachment> {$0.attachedTo == nil}) var loseAttachments: [MemoryAttachment]
-    
+
+    @Query(filter: #Predicate<MemoryAttachment> { $0.attachedTo == nil }) var looseAttachments: [MemoryAttachment]
+    @Query var allAttachments: [MemoryAttachment]
+
+    func cleanLooseAttachments() {
+        // Delete Lose Attachments
+        for attachment in looseAttachments {
+            attachment.deleteFromFileManager()
+            modelContext.delete(attachment)
+        }
+
+        // Check if there are files that aren't linked to a attachment
+        let fm = FileManager.default
+
+        if let baseURL = fm.urls(for: .documentDirectory, in: .userDomainMask).first {
+            do {
+                let contents = try fm.contentsOfDirectory(atPath: baseURL.path())
+
+                for idString in contents {
+                    let id = UUID(uuidString: idString)!
+
+                    if !allAttachments.contains(where: { $0.id == id }) {
+                        let fileURL = baseURL.appendingPathComponent(idString)
+                        try FileManager.default.removeItem(at: fileURL)
+                    }
+                }
+                print(contents)
+            } catch {
+                print("Error on cleaning loose attachments: \(error.localizedDescription)")
+            }
+        }
+
+        try? modelContext.save()
+    }
+
     var body: some View {
         TabView {
-//            Tab("Visão Geral", systemImage: "tray.full") {
-//                NavigationStack {
-//                    OverviewView()
-//                }
-//            }
+            //            Tab("Visão Geral", systemImage: "tray.full") {
+            //                NavigationStack {
+            //                    OverviewView()
+            //                }
+            //            }
 
             Tab("Memórias", systemImage: "photo.stack") {
                 NavigationStack {
@@ -34,12 +66,6 @@ struct TabBar: View {
             }
         }
         .tint(.solidPurple)
-        .onAppear{
-            for loseAttachment in loseAttachments {
-                loseAttachment.deleteFromFileManager()
-                modelContext.delete(loseAttachment)
-            }
-        }
     }
 }
 
